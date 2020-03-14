777466c8fb4a812027024bad9b7762b7c98bcd64
Jason Keeling <s2jason@yahoo.com>
2020-03-14 19:39:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,8 +13,9 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/[A-Z].+\./)
     return true
-  end
+  elsif !!text.match(/[A-Z].+\./)
     return false
+  end
 end
 
 def valid_phone_number?(phone)
