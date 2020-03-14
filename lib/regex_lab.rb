a4811c1033a67b8f3db7fdc59bbbd2927c8806f1
Jason Keeling <s2jason@yahoo.com>
2020-03-14 19:16:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,8 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if text.scan (/b\d{3}.?\d{3}.?\d{4}\b/)
-    return true
-  else
-    return false
+  text.scan (/b\d{3}.?\d{3}.?\d{4}\b/)
 end
