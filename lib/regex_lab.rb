0f0bf09f64c290914fccfb98cab4a11ac3981f47
Jason Keeling <s2jason@yahoo.com>
2020-03-14 20:18:14 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  !!word.capitalize.match(/\A+[AEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/[u][n]\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\b\d{3}.?\d{3}.?\d{4}\b/)
 end
